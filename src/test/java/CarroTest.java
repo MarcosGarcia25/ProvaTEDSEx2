453dd15abe0c2c9e00e8ad3ace2dc9b2e996554d
Marcos Paulo Marçal Garcia <marcos.mgarcia5@gmail.com>
2018-10-11 22:08:11 -0300

    Final - nao realizado

---
+++ src/test/java/CarroTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import javafx.beans.binding.When;
+import modelo.Carro;
+
+import static org.mockito.Mockito.*;
+
+class CarroTest {
+	
+	private Carro carro;
+	
+	@Test
+	void test() {
+		carro.getValorAPagar();
+	}
+
+}
